26f667713eca187e07e9d7e5c1c00e6fe568b9cc
jhmkbry <johnmarkboragay1534@gmail.com>
2025-12-03 20:53:45 +0800

    2nd

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const music = document.getElementById("bgMusic");
 
-// ❄ Create Snowflakes
+
 function createSnow() {
   const snow = document.getElementById("snow");
   const flake = document.createElement("div");
@@ -11,14 +11,11 @@ function createSnow() {
   flake.style.fontSize = Math.random() * 15 + 10 + "px";
   flake.style.animationDuration = Math.random() * 3 + 2 + "s";
 
-  flake.addEventListener("click", enableMusic, { once: true });
-  flake.addEventListener("touchstart", enableMusic, { once: true });
-
   snow.appendChild(flake);
   setTimeout(() => flake.remove(), 5000);
 }
-setInterval(createSnow, 100);
 
+setInterval(createSnow, 100);
 
 function enableMusic() {
   music.volume = 1;
@@ -28,10 +25,12 @@ function enableMusic() {
     console.log("Music playback blocked:", err);
   });
 
+ 
   document.removeEventListener("click", enableMusic);
   document.removeEventListener("touchstart", enableMusic);
   document.removeEventListener("scroll", enableMusic);
 }
+
 
 document.addEventListener("click", enableMusic, { once: true });
 document.addEventListener("touchstart", enableMusic, { once: true });
